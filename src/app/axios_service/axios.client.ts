4429a2d331c5a1a62cfea1547b2b8f1d7014b942
Gianella Rocha <gianellarochavendivengo123@gmail.com>
2025-11-11 13:34:43 -0300

    base para conectar con back

---
+++ src/app/axios_service/axios.client.ts
@@ -0,0 +1,102 @@
+import axios from 'axios';
+import { config } from './env';
+//promociones
+export const axiosAPIPromociones = axios.create({
+  baseURL: config.APIPromocionesUrls.baseUrl,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+axiosAPIPromociones.interceptors.request.use((config) => {
+  const token = localStorage.getItem('access_token');
+  const refreshToken = localStorage.getItem('refresh_token');
+  if (token) {
+    config.headers.Authorization = token;
+  }
+  if (refreshToken) {
+    config.headers['refresh-token'] = refreshToken;
+  }
+
+  return config;
+});
+//peliculas
+export const axiosAPIPeliculas = axios.create({
+  baseURL: config.APIPeliculasUrls.baseUrl,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+axiosAPIPeliculas.interceptors.request.use((config) => {
+  const token = localStorage.getItem('access_token');
+  const refreshToken = localStorage.getItem('refresh_token');
+  if (token) {
+    config.headers.Authorization = token;
+  }
+  if (refreshToken) {
+    config.headers['refresh-token'] = refreshToken;
+  }
+
+  return config;
+});
+//usuario
+export const axiosAPIUsuario = axios.create({
+  baseURL: config.APIUsuarioUrls.baseUrl,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+axiosAPIUsuario.interceptors.request.use((config) => {
+  const token = localStorage.getItem('access_token');
+  const refreshToken = localStorage.getItem('refresh_token');
+  if (token) {
+    config.headers.Authorization = token;
+  }
+  if (refreshToken) {
+    config.headers['refresh-token'] = refreshToken;
+  }
+
+  return config;
+});
+//funciones
+export const axiosAPIFuncionesYsalas = axios.create({
+  baseURL: config.APIFuncionesUrls.baseUrl,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+axiosAPIFuncionesYsalas.interceptors.request.use((config) => {
+  const token = localStorage.getItem('access_token');
+  const refreshToken = localStorage.getItem('refresh_token');
+  if (token) {
+    config.headers.Authorization = token;
+  }
+  if (refreshToken) {
+    config.headers['refresh-token'] = refreshToken;
+  }
+
+  return config;
+});
+//ventas
+export const axiosAPIVentas = axios.create({
+  baseURL: config.APIVentasUrls.baseUrl,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+axiosAPIVentas.interceptors.request.use((config) => {
+  const token = localStorage.getItem('access_token');
+  const refreshToken = localStorage.getItem('refresh_token');
+  if (token) {
+    config.headers.Authorization = token;
+  }
+  if (refreshToken) {
+    config.headers['refresh-token'] = refreshToken;
+  }
+
+  return config;
+});
---
+++ src/app/axios_service/env.ts
@@ -0,0 +1,17 @@
+export const config = {
+  APIPromocionesUrls: {
+    baseUrl: 'http://localhost:3005',
+  },
+  APIFuncionesUrls: {
+    baseUrl: 'http://localhost:3003',
+  },
+  APIUsuarioUrls: {
+    baseUrl: 'http://localhost:3004',
+  },
+  APIPeliculasUrls: {
+    baseUrl: 'http://localhost:3001',
+  },
+  APIVentasUrls: {
+    baseUrl: 'http://localhost:3002',
+  },
+};
